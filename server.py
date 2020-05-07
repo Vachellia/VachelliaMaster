d1d5473f6cd320e51a0dfedd6e4b13de0c74a354
Vachellia <51047991+futureenvision@users.noreply.github.com>
2020-05-07 22:25:18 +0200

    added cool intro 😄. changed test2 method return.

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 import pika
 from core.vachellia import Vachellia, get_json
+from termcolor import colored
 
+print(
+    colored(
+        """
+██    ██  █████   ██████ ██   ██ ███████ ██      ██      ██  █████  
+██    ██ ██   ██ ██      ██   ██ ██      ██      ██      ██ ██   ██ 
+██    ██ ███████ ██      ███████ █████   ██      ██      ██ ███████ 
+ ██  ██  ██   ██ ██      ██   ██ ██      ██      ██      ██ ██   ██ 
+  ████   ██   ██  ██████ ██   ██ ███████ ███████ ███████ ██ ██   ██ 
+                                                                    
+    """
+    )
+)
+print(f"[ {colored('C TYPE', 'green')}  ] -> [ MASTER ]")
+print(f"[ {colored('VERSION', 'green')} ] -> [ 0.01 ]")
 
 vachellia = Vachellia(get_json(r"vachellia.json"))
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="192.168.0.9"))
@@ -10,11 +25,11 @@ channel.queue_declare(queue="client")
 
 
 def callback(ch, method, properties, in_data):
-    # print(f"[🌿][callback][in_data] -> [ {in_data} ]")
+    # print(f"[{colored('OK', 'green')}][callback][in_data] -> [ {in_data} ]")
     out_data = vachellia.operate_request(in_data)
     channel.basic_publish(exchange="", routing_key="client", body=out_data)
 
 
 channel.basic_consume(queue="master", on_message_callback=callback, auto_ack=True)
-print("[🌿][Vachellia Up] ->  [ To exit press CTRL+C ]")
+print(f"[ {colored('STARTED', 'green')} ] -> [ To exit press CTRL+C ]")
 channel.start_consuming()
--- src/router.py
+++ src/router.py
@@ -12,17 +12,17 @@ class Router(object):
     def __init__(self):
         self.class_instance_name = "Router"
         self.request_manager = RequestManager()
-        self.new_request = self.request_manager.generate_request()
 
     def goto(self, parameters):
+        self.new_request = self.request_manager.generate_request(parameters[2])
         print("[Router][test][parameters] -> ", parameters)
         self.router = RemoteClass("Temp", self.new_request)
         for procedure_call in parameters[1]:
             self.router.append_procedure_call(procedure_call)
 
         out_data = self.request_manager.compute_resolve(self.new_request, self.test2)
+        print("[Router][test][remote_request] -> ", out_data)
         base64_out_data = base64.b64encode(json.dumps(out_data).encode())
-        print("[Router][test][remote_request] -> ", base64_out_data)
         channel.basic_publish(exchange="", routing_key="cell_1", body=base64_out_data)
 
         return {
@@ -33,8 +33,4 @@ class Router(object):
 
     def test2(self, data):
         print(f"[test2] ->", data)
-        return {
-            "status": "pending",
-            "data": "request sent to ",
-            "class_name": self.class_instance_name,
-        }
+        return [data, data]
